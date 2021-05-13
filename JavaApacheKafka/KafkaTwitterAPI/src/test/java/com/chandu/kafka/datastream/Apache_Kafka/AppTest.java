3dcffd12d6bc8688b16be5f750b5fc2103255e7e
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2021-05-12 23:20:14 -0400

    Java Apache Kafka

---
+++ JavaApacheKafka/KafkaTwitterAPI/src/test/java/com/chandu/kafka/datastream/Apache_Kafka/AppTest.java
@@ -0,0 +1,20 @@
+package com.chandu.kafka.datastream.Apache_Kafka;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ JavaApacheKafka/kafka-connect/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# run the twitter Connector
+#connect-standalone connect-standalone.properties twitter.properties
+# OR (linux or MAC OSX)
+connect-standalone.sh connect-standalone.properties twitter.properties
+# OR Windows
+#connect-standalone.bat connect-standalone.properties twitter.properties
